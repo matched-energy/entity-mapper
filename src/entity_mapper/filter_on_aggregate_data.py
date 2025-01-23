4e2df82ae761f899ac68fdf234c23632f061e553
Joe Kwiatkowski <joejk2@gmail.com>
2025-01-23 13:40:54 +0000

    Consolidate functions

--- src/entity_mapper/filter_on_aggregate_data.py
+++ src/entity_mapper/filter_on_aggregate_data.py
@@ -15,19 +15,29 @@ def appraise_rated_power(generator_profile: dict) -> dict:
     )
 
 
-def compare_rego_and_bmu_volumes(rego_volumes: pd.DataFrame, bmu_volumes: pd.DataFrame) -> pd.DataFrame:
-    merged_df = pd.merge(
+def appraise_energy_volumes(generator_profile: dict, regos: pd.DataFrame) -> dict:
+    rego_volume_stats, rego_volumes = extract_rego_volume(
+        regos,
+        generator_profile["rego_station_name"],
+        generator_profile["rego_station_dnc_mw"],
+    )
+    generator_profile.update(rego_volume_stats)
+
+    bmu_volume_stats, bmu_volumes = extract_bm_vols_by_month(
+        generator_profile["bmu_lead_party_id"],
+        [bmu["bmu_unit"] for bmu in generator_profile["bmus"]],
+        generator_profile["bmus_total_net_capacity"],
+    )
+    generator_profile.update(bmu_volume_stats)
+
+    volume_comparison = pd.merge(
         rego_volumes,
         bmu_volumes,
         left_index=True,
         right_index=True,
     )
-    merged_df["rego_to_bmu_ratio"] = merged_df["GWh"] / merged_df["BM Unit Metered Volume"]
-    merged_df.index.name = "start"
-    return merged_df
-
-
-def parse_volume_comparison(volume_comparison: pd.DataFrame) -> dict:
+    volume_comparison["rego_to_bmu_ratio"] = volume_comparison["GWh"] / volume_comparison["BM Unit Metered Volume"]
+    volume_comparison.index.name = "start"
     try:
         return dict(
             volume_periods=[
@@ -45,23 +55,4 @@ def parse_volume_comparison(volume_comparison: pd.DataFrame) -> dict:
             rego_bmu_volume_ratio_max=volume_comparison["rego_to_bmu_ratio"].max(),
         )
     except Exception as e:
-        raise MappingException(e)
-
-
-def appraise_energy_volumes(generator_profile: dict, regos: pd.DataFrame) -> dict:
-    rego_volume_stats, rego_volumes = extract_rego_volume(
-        regos,
-        generator_profile["rego_station_name"],
-        generator_profile["rego_station_dnc_mw"],
-    )
-    generator_profile.update(rego_volume_stats)
-
-    bmu_volume_stats, bmu_volumes = extract_bm_vols_by_month(
-        generator_profile["bmu_lead_party_id"],
-        [bmu["bmu_unit"] for bmu in generator_profile["bmus"]],
-        generator_profile["bmus_total_net_capacity"],
-    )
-    generator_profile.update(bmu_volume_stats)
-
-    volume_comparison = compare_rego_and_bmu_volumes(rego_volumes, bmu_volumes)
-    return parse_volume_comparison(volume_comparison)
+        raise MappingException(str(e))
