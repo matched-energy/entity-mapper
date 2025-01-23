bc0b325c55bf9caeec8e1a26bbebebc86c875801
Joe Kwiatkowski <joejk2@gmail.com>
2025-01-23 13:28:38 +0000

    Reorder contents of file

--- src/entity_mapper/match_meta_data.py
+++ src/entity_mapper/match_meta_data.py
@@ -1,19 +1,12 @@
 import numpy as np
 import pandas as pd
 
-from entity_mapper.common import MappingException
 
-
-def apply_bmu_match_filters(bmus: pd.DataFrame, filters: list) -> pd.DataFrame:
-    filtered_bmus = bmus.loc[np.logical_and.reduce(filters)]
-
-    try:
-        assert len(filtered_bmus) > 0
-    except AssertionError:
-        warning = "No matching BMUs found"
-        raise MappingException(warning)
-
-    return filtered_bmus
+################################################################################
+# HELPER FUNCTIONS
+################################################################################
+def words(name: str) -> list:
+    return [] if name is None else [word.strip("()") for word in name.lower().split()]
 
 
 def contiguous_words(l_name: str, r_name: str) -> int:
@@ -24,15 +17,6 @@ def contiguous_words(l_name: str, r_name: str) -> int:
         else:
             break
     return count
-
-
-def filter_on_name_contiguous(station_profile: dict, bmus: pd.DataFrame) -> (pd.Series, pd.Series):
-    lead_party_count = bmus["leadPartyName"].apply(lambda x: contiguous_words(station_profile["rego_station_name"], x))
-    bmu_count = bmus["bmUnitName"].apply(lambda x: contiguous_words(station_profile["rego_station_name"], x))
-    # max_count = pd.Series(map(max, lead_party_count, bmu_count))
-    max_count = pd.Series([max(x, y) for x, y in zip(lead_party_count, bmu_count)])
-    max_count_filter = (max_count > 0) & (max_count == max(max_count))
-    return max_count, max_count_filter
 
 
 def intersection(series: pd.Series, value: str, ignore: set = None) -> (pd.Series, pd.Series):
@@ -47,6 +31,18 @@ def intersection(series: pd.Series, value: str, ignore: set = None) -> (pd.Serie
     )
     max_count_filter = (intersection_count > 0) & (intersection_count == max(intersection_count))
     return intersection_count, max_count_filter
+
+
+################################################################################
+# FILTERS
+################################################################################
+def filter_on_name_contiguous(station_profile: dict, bmus: pd.DataFrame) -> (pd.Series, pd.Series):
+    lead_party_count = bmus["leadPartyName"].apply(lambda x: contiguous_words(station_profile["rego_station_name"], x))
+    bmu_count = bmus["bmUnitName"].apply(lambda x: contiguous_words(station_profile["rego_station_name"], x))
+    # max_count = pd.Series(map(max, lead_party_count, bmu_count))
+    max_count = pd.Series([max(x, y) for x, y in zip(lead_party_count, bmu_count)])
+    max_count_filter = (max_count > 0) & (max_count == max(max_count))
+    return max_count, max_count_filter
 
 
 def filter_on_name_intersection(station_profile: dict, bmus: pd.DataFrame) -> (pd.Series, pd.Series):
@@ -79,6 +75,9 @@ def filter_on_generation_capacity(station_profile: dict, bmus: pd.DataFrame) -> 
     )
 
 
+################################################################################
+# APPLY FILTERS
+################################################################################
 def define_bmu_match_features_and_filters(station_profile: dict, bmus: pd.DataFrame) -> (pd.DataFrame, list):
     features = pd.DataFrame(index=bmus.index)
     filters = []
@@ -100,5 +99,13 @@ def define_bmu_match_features_and_filters(station_profile: dict, bmus: pd.DataFr
     return features, filters
 
 
-def words(name: str) -> list:
-    return [] if name is None else [word.strip("()") for word in name.lower().split()]
+def apply_bmu_match_filters(bmus: pd.DataFrame, filters: list) -> pd.DataFrame:
+    filtered_bmus = bmus.loc[np.logical_and.reduce(filters)]
+
+    try:
+        assert len(filtered_bmus) > 0
+    except AssertionError:
+        warning = "No matching BMUs found"
+        raise MappingException(warning)
+
+    return filtered_bmus
